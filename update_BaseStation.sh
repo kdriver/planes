733a81217549185126523792b84a62e4468cd4a7
keith <keith@just-the-one.co.uk>
2020-03-06 09:58:39 +0000

    fixed update of basestation.sqb to use gzip

--- update_BaseStation.sh
+++ update_BaseStation.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-remote=`curl --silent --head  http://data.flightairmap.com/data/basestation/BaseStation.sqb.zip  | grep Last | sed -e 's/Last-Modified://'`
+remote=`curl --silent --head  http://data.flightairmap.com/data/basestation/BaseStation.sqb.gz  | grep Last | sed -e 's/Last-Modified://'`
 trem=`date --date="$remote" +%s`
-if test -f ./BaseStation.sqb.zip ; then
-	echo "file  BaseStation.sqb.zip exists"
-	loc=`stat -c %z "BaseStation.sqb.zip"`
+if test -f ./BaseStation.sqb.gz ; then
+	echo "file  BaseStation.sqb.gz exists"
+	loc=`stat -c %y "BaseStation.sqb.gz"`
 	tloc=`date --date="$loc" +%s`
 	echo  "local  ${tloc}",`date -d@$tloc`
 	echo  "remote ${trem}",`date -d@$trem`
@@ -11,16 +11,16 @@ if test -f ./BaseStation.sqb.zip ; then
 		echo "Download it "
 		echo "remote $remote"
 		echo "local $loc"
-		curl -o BaseStation.sqb.zip http://data.flightairmap.com/data/basestation/BaseStation.sqb.zip  
+		curl -o BaseStation.sqb.gz http://data.flightairmap.com/data/basestation/BaseStation.sqb.gz  
 		echo "unzip it"
-		unzip BaseStation.sqb.zip
-		touch -d "@$trem" ./BaseStation.sqb.zip
+		gunzip -f --keep BaseStation.sqb.gz
+		touch -d "@$trem" ./BaseStation.sqb.gz
 	else
 		echo "It hasnt been updated so do nothing "
 	fi
 else
 	echo "File doesnt exist - redownload it "
-	curl -o BaseStation.sqb.zip http://data.flightairmap.com/data/basestation/BaseStation.sqb.zip  && unzip -o BaseStation.sqb.zip
-	touch -d "@$trem" ./BaseStation.sqb.zip
+	curl -o BaseStation.sqb.gz http://data.flightairmap.com/data/basestation/BaseStation.sqb.gz  && gunzip  -f --keep BaseStation.sqb.gz
+	touch -d "@$trem" ./BaseStation.sqb.gz
 fi
 exit 0
